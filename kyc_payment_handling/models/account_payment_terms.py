c851d6b9a5bcc6e37ebb4cddb6fde2c2a9c72959
SAYF <nrj0263@gmail.com>
2025-10-03 14:37:48 +0500

    KYC - Contract Template Updates

--- kyc_payment_handling/models/account_payment_terms.py
+++ kyc_payment_handling/models/account_payment_terms.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class AccountPaymentTerm(models.Model):
     _inherit = "account.payment.term"
 
-    installment_amount = fields.Float(string="Installment Extra Amount")
+    installment_amount = fields.Float(string="Installment Markup Amount")
