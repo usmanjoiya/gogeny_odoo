8c26f5ee1ab7757c08192a3a0d21265da43316f0
SAYF <nrj0263@gmail.com>
2025-10-28 01:50:21 +0500

    Menifest Updates

--- kyc_payment_handling/__manifest__.py
+++ kyc_payment_handling/__manifest__.py
@@ -8,7 +8,7 @@
     'category': 'Uncategorized',
     'version': '0.1',
 
-    'depends': ['base', 'website', 'account', 'website_sale', 'project', 'web_editor', 'payment',],
+    'depends': ['base', 'website', 'account', 'website_sale', 'project', 'web_editor', 'payment', 'payment_custom'],
     'data': [
         # 'security/security.xml',
         'security/ir.model.access.csv',
